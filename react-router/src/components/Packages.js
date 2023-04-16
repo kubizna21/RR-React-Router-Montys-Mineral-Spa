679dca300b3752f53874ef5de1ac7273a615406a
Ashley Kubizna <113566558+kubizna21@users.noreply.github.com>
2023-04-16 11:56:17 -0700

    added changes

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,14 +1,19 @@
 
 
-export default function Packages() {
-
+export default function Packages({ packages }) {
+    const displayPackages = packages.map( eachPackage => <li>{ eachPackage }</li>)
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                <ul>
+                    {
+                        displayPackages
+                    }
+                </ul>
+
             </div>
         </div>
     )
